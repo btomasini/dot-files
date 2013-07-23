63c74cee12f08a43ad2dc089fca429c86d2575b1
Ben Tomasini <ben.tomasini@gmail.com>
2013-07-22 19:46:01 -0700

    Added install script and vim-scala

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Installs dot files after backing up current files
+
+function backup_and_copy {
+
+file=$1
+
+existing="$HOME/$file"
+
+if [[ -e "$existing" ]]
+then
+  mv $existing $backupdir
+fi
+
+if [[ -d "$file" ]] 
+then
+  cp -R $file $HOME
+else
+  cp $file $HOME
+fi
+
+}
+
+now=`date +%s`
+cwd=$(pwd)
+backupdir=~/.dotbackup/$now
+
+if [[ ! -e "$cwd/.vim" ]] 
+then
+  echo "Please execute install from within the project directory"
+  exit 1
+fi
+
+mkdir -p $backupdir
+
+backup_and_copy .vimrc
+backup_and_copy .vim
+
+
